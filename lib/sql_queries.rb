881d4f6c042b8ad899adfc12293ac97f89f3614d
Allison _ <allimacq@gmail.com>
2021-01-05 03:42:19 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,7 +35,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     LEFT OUTER JOIN projects
     ON pledges.project_id = projects.id
     GROUP BY projects.title 
-    HAVING (SELECT SUM(amount) FROM pledges GROUP BY project_id) >= (SELECT projects.funding_goal)"
+    "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
@@ -47,7 +47,9 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
     FROM projects
     LEFT OUTER JOIN pledges
     ON projects.id = pledges.project_id
-    WHERE (SELECT SUM(amount) FROM pledges GROUP BY project_id) - (SELECT projects.funding_goal) >= 0"
+    WHERE (SELECT SUM(amount) FROM pledges GROUP BY project_id) - (SELECT projects.funding_goal) >= 0
+    
+    HAVING (SELECT SUM(amount) FROM pledges GROUP BY project_id) >= (SELECT projects.funding_goal)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
