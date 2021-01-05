2cfff6f2a8f4c0314ce0c5078532223cc996f17a
Allison _ <allimacq@gmail.com>
2021-01-05 03:38:33 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -30,7 +30,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT 
     projects.title,
-    projects.id
+    pledges.projects_id,
     (SELECT SUM(amount) amount FROM pledges GROUP BY project_id)
     FROM pledges
     LEFT OUTER JOIN projects
