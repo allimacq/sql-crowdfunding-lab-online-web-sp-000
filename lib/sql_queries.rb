b4af4b8a7079f86241fdf689c96bdb0d986abd3a
Allison _ <allimacq@gmail.com>
2021-01-05 04:06:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,11 +41,11 @@ end
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT
     users.name,
-    (SELECT ORDER BY (SUM(pledges.amount)))
+    (SELECT SUM(pledges.amount))
     FROM pledges
     LEFT OUTER JOIN users
     ON pledges.user_id = users.id
-    GROUP BY users.name"
+    GROUP BY users.name, SUM(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
