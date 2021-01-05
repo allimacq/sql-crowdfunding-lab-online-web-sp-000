b425f4816eebe0973144ca0178d15f032f978781
Allison _ <allimacq@gmail.com>
2021-01-05 03:57:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -44,7 +44,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
     FROM pledges
     LEFT OUTER JOIN users
     ON pledges.user_id = users.id
-    GROUP BY pledges.user_id"
+    GROUP BY pledges.users_id"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
