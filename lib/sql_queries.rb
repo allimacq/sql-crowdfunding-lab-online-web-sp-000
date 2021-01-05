da899c496b7ca138ca47bae4e1fed8fb7764239d
Allison _ <allimacq@gmail.com>
2021-01-05 03:35:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     projects.title,
     (SELECT SUM(amount) amount FROM pledges GROUP BY project_id)
     FROM projects
-    LEFT OUTER JOIN pledges
+    INNER JOIN pledges
     ON projects.id = pledges.project_id
     GROUP BY pledges.amount 
     HAVING (SELECT SUM(amount) FROM pledges GROUP BY project_id) - (SELECT projects.funding_goal) >= 0"
